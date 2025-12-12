9834ac50a3a35b3b8b549282bca96974320ded4f
konstant.gka@gmail.com <konstant.gka@gmail.com>
2025-12-12 20:26:17 +0100

    Upd

--- SQL/Covid_Deaths_Queries.sql
+++ SQL/Covid_Deaths_Queries.sql
@@ -75,7 +75,102 @@ where continent is not null
 group by date
 order by 1,2
 
+
+-- Excel Tables for use into Tableau Desktop
+
+--1.
+
 select SUM(new_cases) as TotalCases, SUM(new_deaths) as TotalDeaths, SUM(new_deaths)/SUM(NULLIF(new_cases,0)) * 100 as DeathPercentage
 from Covid_DB..['Covid_Deaths_2020-2023$']
 where continent is not null
 order by 1,2
+
+--2
+
+select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+from Covid_DB..['Covid_Deaths_2020-2023$'] 
+where continent is null
+and location not in 
+(
+'World', 
+'European Union', 
+'International', 
+'World excl. China',
+'World excl. China and South Korea', 
+'World excl. China, South Korea, Japan and Singapore', 
+'Asia excl. China',
+'European Union (27)',
+'Lower-middle-income countries',
+'Low-income countries',
+'Summer Olympics 2020',
+'Northern Ireland',
+'England and Wales',
+'Scotland',
+'Winter Olympics 2022',
+'Wales',
+'England',
+'High-income countries',
+'Upper-middle-income countries'
+)
+group by location
+order by TotalDeathCount desc
+
+
+--3
+
+select location, population, MAX(total_cases) as HighInfectionCount, Max((total_cases/population))*100 as PercentPopulationInfected
+from Covid_DB..['Covid_Deaths_2020-2023$']
+where location not in 
+(
+'World', 
+'European Union', 
+'International', 
+'World excl. China',
+'World excl. China and South Korea', 
+'World excl. China, South Korea, Japan and Singapore', 
+'Asia excl. China',
+'European Union (27)',
+'Lower-middle-income countries',
+'Low-income countries',
+'Summer Olympics 2020',
+'Northern Ireland',
+'England and Wales',
+'Scotland',
+'Winter Olympics 2022',
+'Wales',
+'England',
+'High-income countries',
+'Upper-middle-income countries'
+)
+group by location, population
+order by PercentPopulationInfected desc
+
+
+--4
+
+select location, population, date, MAX(total_cases) as HighInfectionCount, MAX((total_cases/population))*100 as PercentPopulationInfected
+from Covid_DB..['Covid_Deaths_2020-2023$'] 
+where location not in 
+(
+'World', 
+'European Union', 
+'International', 
+'World excl. China',
+'World excl. China and South Korea', 
+'World excl. China, South Korea, Japan and Singapore', 
+'Asia excl. China',
+'European Union (27)',
+'Lower-middle-income countries',
+'Low-income countries',
+'Summer Olympics 2020',
+'Northern Ireland',
+'England and Wales',
+'Scotland',
+'Winter Olympics 2022',
+'Wales',
+'England',
+'High-income countries',
+'Upper-middle-income countries'
+)
+group by location, population, date
+order by PercentPopulationInfected desc
